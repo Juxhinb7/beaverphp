64bd6a52e097f0646c0ee0b561a4c8c1769332d9
juxhinb7 <juxhinbazelli@gmail.com>
2024-10-15 22:22:03 +0200

    Added crafter config

---
+++ src/Config/Singletons/CrafterSingleton.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Beaver\Config\Singletons;
+
+final class CrafterSingleton
+{
+    private static $instance;
+    private function __construct()
+    {
+
+    }
+
+    public static function makeSingle(): CrafterSingleton
+    {
+        if (!isset(CrafterSingleton::$instance)) {
+            CrafterSingleton::$instance =  new CrafterSingleton();
+        }
+        return CrafterSingleton::$instance;
+    }
+
+    public function generateFile($fileName = 'crafter'): void
+    {
+        file_put_contents($fileName . '.php', "<?php\n\n" . 'class Crafter{public function execute($args){if (explode(":", $args[1])[0] === "make"){echo "made";}}}(new Crafter())->execute($argv);');
+    }
+
+}
