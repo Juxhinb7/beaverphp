8f38743ffbecf00770c8dc9f81126dc3f1e4a741
juxhinb7 <juxhinbazelli@gmail.com>
2024-10-15 22:38:05 +0200

    new one

--- src/Config/Singletons/CrafterSingleton.php
+++ src/Config/Singletons/CrafterSingleton.php
@@ -20,17 +20,7 @@ final class CrafterSingleton
 
     public function generateFile($fileName = 'crafter'): void
     {
-        file_put_contents($fileName, "<?php\n\n" . '
-        class Crafter{
-            public function execute($args)
-            {
-                if (explode(":", $args[1])[0] === "make"){
-                    echo "made";
-                }
-            }
-        }
-        
-        (new Crafter())->execute($argv);');
+        file_put_contents($fileName, "<?php\n\n" . 'class Crafter{public function execute($args){if (explode(":", $args[1])[0] === "make"){echo "made";}}}(new Crafter())->execute($argv);');
     }
 
 }
