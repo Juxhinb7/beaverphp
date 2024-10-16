ff4ddeae454dae214bf78daea2c6e0c6204d6a5d
juxhinb7 <juxhinbazelli@gmail.com>
2024-10-16 21:19:18 +0200

    refactor: 6.0

--- src/Config/Singletons/CrafterSingleton.php
+++ src/Config/Singletons/CrafterSingleton.php
@@ -25,39 +25,32 @@ final class CrafterSingleton
 class Crafter{
     public function execute($args)
     {
-        try {
-            if (explode(":", $args[1])[0] === "make"){
+        if (explode(":", $args[1])[0] === "make"){
 
-                $fileType = explode(":", $args[1])[1];
-    
-                match($fileType) {
-                    "controller" => (function() use($args){
-                        if (!is_dir("controllers")) {
-                            mkdir("controllers");
-                        }
-    
-                        if ($args[2]) {
-                            file_put_contents("controllers/" . $args[2] . ".php", sprintf("
-    <?php
-    
-    class %s
-    {
-    
-    }
-                            ", $args[2]));
-                        }
-                    })(),
-                    default => (function(){
-                        echo "Cannot process file type." . PHP_EOL;
-                    })()
-                };
-            }
+            $fileType = explode(":", $args[1])[1];
+
+            match($fileType) {
+                "controller" => (function() use($args){
+                    if (!is_dir("controllers")) {
+                        mkdir("controllers");
+                    }
+
+                    if ($args[2]) {
+                        file_put_contents("controllers/" . $args[2] . ".php", sprintf("
+<?php
+
+class %s
+{
+
+}
+                        ", $args[2]));
+                    }
+                })(),
+                default => (function(){
+                    echo "Cannot process file type." . PHP_EOL;
+                })()
+            };
         }
-        catch(\Exception $e)
-        {
-            echo "No arguments were given." . PHP_EOL;
-        }
-        
     }
 }
 (new Crafter())->execute($argv);');
